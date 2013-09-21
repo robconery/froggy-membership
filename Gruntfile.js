e6c5032352d03ddee92413b092c00f4986daab00
Rob Conery <rob@wekeroad.com>
2013-09-20 15:58:09 -1000

    Initial

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    jshint : {
+      files : ['lib/**/*js', 'models/**/*.js']
+    },
+    watch : {
+      files: ['lib/**/*js', 'models/**/*.js'],
+      tasks : ['jshint']
+    }
+
+
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+}
