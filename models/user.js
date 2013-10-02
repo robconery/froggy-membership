35e568005908603d2533404849ea26ddda9d3692
Rob Conery <rob@wekeroad.com>
2013-10-02 10:15:31 -1000

    Auth

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ var utility = require("../lib/utility");
 var User = function(args){
   assert.ok(args.email, "Email is required");
   var user = {};
-
+  if(args.id){
+    user.id = args.id;
+  }
   user.email = args.email;
   user.createdAt = args.createdAt || new Date();
   user.status = args.status || "pending";
@@ -13,6 +15,7 @@ var User = function(args){
   user.currentLoginAt = args.currentLoginAt || new Date();
   user.authenticationToken = args.authenticationToken || utility.randomString(18);
   user.hashedPassword = args.hashedPassword || null;
+
   return user;
 
 };
